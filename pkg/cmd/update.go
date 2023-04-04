463e2dc1dce87773525411670fd88fc17053096b
Shaharia Azam <mail@shaharia.com>
2023-04-04 22:58:53 +0200

    Removed un-necessary code for now

--- pkg/cmd/update.go
+++ pkg/cmd/update.go
@@ -3,18 +3,13 @@ package cmd
 
 import (
 	"bd-stock-market/pkg/stock"
-	"encoding/json"
 	"fmt"
 	"github.com/spf13/cobra"
-	"io/ioutil"
 )
 
 // NewUpdateCommand build "display" name
 func NewUpdateCommand() *cobra.Command {
-	var outputFormat string
-	var outputFile string
 	var collectionBatchSize int
-	var verbose bool
 
 	cmd := cobra.Command{
 		Use:   "update",
@@ -23,11 +18,6 @@ func NewUpdateCommand() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			st := stock.NewStock("https://www.dsebd.org", false)
 			stockData, err := st.GetData(collectionBatchSize)
-			if err != nil {
-				return err
-			}
-
-			err = processJSONOutput(outputFormat, stockData, outputFile)
 			if err != nil {
 				return err
 			}
@@ -41,29 +31,5 @@ func NewUpdateCommand() *cobra.Command {
 		},
 	}
 
-	cmd.PersistentFlags().StringVarP(&outputFormat, "format", "c", "", "Display format. eg: json, table")
-	cmd.PersistentFlags().StringVarP(&outputFile, "file", "f", "", "File path to save the output")
-	cmd.PersistentFlags().IntVarP(&collectionBatchSize, "batch-size", "b", 10, "Batch size to fetch data concurrently")
-	cmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Verbose output")
-
 	return &cmd
-}
-
-func processJSONOutput(outputFormat string, stockData []stock.CompanyStockData, outputFile string) error {
-	if outputFormat == "json" {
-		jsonData, err := json.MarshalIndent(stockData, "", "  ")
-		if err != nil {
-			return fmt.Errorf("error encoding JSON")
-		}
-
-		if outputFile != "" {
-			err = ioutil.WriteFile(outputFile, jsonData, 0644)
-			if err != nil {
-				return fmt.Errorf("error writing file: %w", err)
-			}
-
-			fmt.Println("data has been saved")
-		}
-	}
-	return nil
 }
